fe54d93a96efc86ed3994c3df7ba0bb07ece7617
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-26 18:30:48 -0500

    added players to firebase and referenced them on refresh

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,16 +11,29 @@ firebase.initializeApp(config);
 //Creating an instance of firebase to reference
 var database = firebase.database();
 var refPlayers = database.ref('players');
-var players = false;
+var playerData;
+var player1;
+var player2;
 //Getting a snapshot of the local data
 //This function allows me to update the page in real-time by adding the 'value' event listener
 refPlayers.on('value', function(snapshot){
-	console.log(snapshot);
-	//This checks if there is already data
-	//If there is then player will now equal 2
-	if(snapshot.val()){
-		players = true;
-		console.log(players);
+	//if there are no players then nothing happens
+	if(!snapshot.val()) {
+		console.log('There is nothing in the database');
+	}
+	//if both players are in the database then this gets run
+	else if(snapshot.val()[1] && snapshot.val()[2]){
+		console.log('Player 1 and 2 exists')
+		player1 = true;
+		player2 = true;
+		
+	}
+	else if(snapshot.val()[1]){
+		console.log('Only player 1 exists');
+		player1 = true;
+	}
+	else {
+		console.log('this should not occur')
 	}
 });
 //This function will update the database with the name of the player 
@@ -29,27 +42,29 @@ $('#startBtn').on('click', function(event){
 	//This declares playerName to be the input value
 	var playerName = $('#playerName').val().trim();
 	// I create an object that will hold his info and pass it on to firebase
-	if (!players){
-		var playerData = {
+	if(!player1){
+		playerData = {
 			1: {
 				lossess: 0,
 				name: playerName,
 				wins: 0
 			}
 		}
+		player1 = true;
 	}
-	else {
-		var playerData = {
+	else if(!player2) {
+		playerData = {
 			2: {
 				lossess: 0,
 				name: playerName,
 				wins: 0
 			}
 		}
+		player2 = true;
+		console.log('creating player 2');
 	}
-	
-	//This sets the first player firebase
 	refPlayers.update(playerData);
 
 })
+
 
