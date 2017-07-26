0b1ac26d2667eb2c7984bf6a781f0f572eb174f8
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-26 15:29:47 -0500

    Wireframe with minor styling

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDMGmU6sAF0gsqozNxxspZVOmE9I7j-e9Y",
+authDomain: "rockpaperscissors-430d4.firebaseapp.com",
+databaseURL: "https://rockpaperscissors-430d4.firebaseio.com",
+projectId: "rockpaperscissors-430d4",
+storageBucket: "",
+messagingSenderId: "954071881956"
+};
+firebase.initializeApp(config);
